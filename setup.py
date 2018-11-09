92672dd7ca5b251f8482068c1e5a06fa62e6baba
Vincent Morello <vmorello@gmail.com>
2018-11-09 22:27:45 +0000

    Fixed URL in setup.py and removed long_description

--- setup.py
+++ setup.py
@@ -4,12 +4,11 @@ from setuptools import setup
 
 setup(
     name='clfd-pulsar',
-    url='https://bitbucket.org/vmorello/clfd',
+    url='https://github.com/v-morello/clfd',
     author='Vincent Morello',
     author_email='vmorello@gmail.com',
     description='Smart RFI removal algorithms to be used on folded pulsar search and timing data',
     version='0.1',
     packages=setuptools.find_packages(),
     license='MIT License',
-    long_description=open('README.md').read(),
 )
