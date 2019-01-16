c945f4f9c0d67660b5b936f4b03013f643e0aeef
Vincent Morello <vmorello@gmail.com>
2019-01-16 15:05:19 +0000

    Updated version number in setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     author='Vincent Morello',
     author_email='vmorello@gmail.com',
     description='Smart RFI removal algorithms to be used on folded pulsar search and timing data',
-    version='0.1.0',
+    version='0.1.1',
     packages=setuptools.find_packages(),
     install_requires=['numpy','pandas'],
     license='MIT License',
