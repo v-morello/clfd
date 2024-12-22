8ca58a6fdac13451b1dcdb8c31588b3b2086818f
Vincent Morello <vmorello@gmail.com>
2024-12-22 14:25:43 +0100

    Start simplifying CLI app code

--- clfd/apps/functions.py
+++ clfd/apps/functions.py
@@ -8,23 +8,17 @@ import clfd.interfaces
 log = logging.getLogger("clfd")
 
 
-def load_zapfile(fname):
-    """Load zapfile into a list of channel indices. If 'fname' is None, return an empty list."""
-    if not fname:
-        log.info("No zapfile specified.")
-        return []
-
-    log.debug("Loading zapfile: {}".format(fname))
-    with open(fname, "r") as fobj:
-        zap_channels = list(map(int, fobj.read().strip().split()))
-    log.debug("Ignoring {:d} channel indices: {!s}".format(len(zap_channels), zap_channels))
-
+def load_zapfile(fname: str) -> list[int]:
+    """Load zapfile into a list of channel indices."""
+    log.debug(f"Loading zapfile: {fname}")
+    with open(fname, "r") as file:
+        zap_channels = list(map(int, file.read().strip().split()))
+    log.debug(f"Ignoring {len(zap_channels)} channel indices: {zap_channels}")
     return zap_channels
 
 
 def cleanup_file(
     fpath,
-    interface,
     zap_channels,
     outdir=None,
     features=("std", "ptp", "lfamp"),
@@ -38,9 +32,10 @@ def cleanup_file(
     # 'fname': file name without base directory
     fpath = os.path.realpath(fpath)
     fdir, fname = os.path.split(fpath)
-    basename, extension = os.path.splitext(fname)
+    basename, __ = os.path.splitext(fname)
 
     log.debug("Processing: {:s}".format(fpath))
+    interface = clfd.interfaces.get_interface("psrchive")
     archive, cube = interface.load(fpath)
 
     log.debug("{:s} data shape: {!s}".format(fname, cube.data.shape))
@@ -71,8 +66,9 @@ def cleanup_file(
         qspike = None
 
     # Save output
-    if outdir is None:
-        outdir = fdir
+    # outdir = None means that data products for a given input archive
+    # are placed in the same directory as the archive itself
+    outdir = os.path.realpath(outdir) if outdir else fdir
     outpath = "{}.{}".format(os.path.join(outdir, fname), ext)
 
     interface.save(outpath, archive)
@@ -90,49 +86,21 @@ def cleanup_file(
         log.debug("Saved report file: {:s}".format(report_path))
 
 
-class CleanupWorker(object):
-    """Function-like object called by multiprocessing.Pool"""
+class Worker:
+    """
+    Function-like object to be applied by a multiprocessing Pool.
+    """
 
-    def __init__(
-        self,
-        interface,
-        zap_channels,
-        outdir=None,
-        features=("std", "ptp", "lfamp"),
-        qmask=2.0,
-        despike=False,
-        qspike=4.0,
-        ext="clfd",
-        report=True,
-    ):
-        self.interface = interface
-        self.zap_channels = zap_channels
-        self.outdir = outdir
-        self.features = features
-        self.qmask = qmask
-        self.despike = despike
-        self.qspike = qspike
-        self.ext = ext
-        self.report = report
+    def __init__(self, cleanup_file_kwargs: dict):
+        self.cleanup_file_kwargs = cleanup_file_kwargs
 
-    def __call__(self, fname):
-        cleanup_file(
-            fname,
-            self.interface,
-            self.zap_channels,
-            outdir=self.outdir,
-            features=self.features,
-            qmask=self.qmask,
-            despike=self.despike,
-            qspike=self.qspike,
-            ext=self.ext,
-            report=self.report,
-        )
+    def __call__(self, fpath: str):
+        kwargs = self.cleanup_file_kwargs | {"fpath": fpath}
+        cleanup_file(**kwargs)
 
 
 def cleanup_main(
     filenames,
-    fmt="psrchive",
     outdir=None,
     zapfile=None,
     features=("std", "ptp", "lfamp"),
@@ -144,36 +112,21 @@ def cleanup_main(
     processes=1,
 ):
     log.debug("Files to process: {:d}".format(len(filenames)))
-    log.debug("Format: {}".format(fmt))
-    interface = clfd.interfaces.get_interface(fmt)
-    zap_channels = load_zapfile(zapfile)
+    zap_channels = load_zapfile(zapfile) if zapfile else []
 
-    # outdir = None means that data products for each input file
-    # are placed in the same directory as that file
-    if outdir is not None:
-        outdir = os.path.realpath(outdir)
+    cleanup_file_kwargs = {
+        "zap_channels": zap_channels,
+        "outdir": outdir,
+        "features": features,
+        "qmask": qmask,
+        "despike": despike,
+        "qspike": qspike,
+        "ext": ext,
+        "report": report,
+    }
+    worker = Worker(cleanup_file_kwargs)
 
-    worker_func = CleanupWorker(
-        interface,
-        zap_channels,
-        outdir=outdir,
-        features=features,
-        qmask=qmask,
-        despike=despike,
-        qspike=qspike,
-        ext=ext,
-        report=report,
-    )
-
-    if processes > 1:
-        log.debug("Using multiprocessing with {:d} processes".format(processes))
-        pool = multiprocessing.Pool(processes=processes)
-        pool.map(worker_func, filenames)
-        pool.close()
-        pool.join()
-    else:
-        log.debug("Processing input files sequentially")
-        for fname in filenames:
-            worker_func(fname)
-
-    log.debug("Done.")
+    log.info("Using {:d} parallel processes".format(processes))
+    with multiprocessing.Pool(processes=processes) as pool:
+        pool.map(worker, filenames)
+    log.info("Done.")
