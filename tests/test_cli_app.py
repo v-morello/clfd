9b6b69795dc00ab4f92bec3da659cfe250f5d4eb
Vincent Morello <vmorello@gmail.com>
2024-12-24 16:13:20 +0100

    Start moving fixtures to conftest.py

--- tests/test_cli_app.py
+++ tests/test_cli_app.py
@@ -18,45 +18,6 @@ def test_cli_app_entrypoint_exists():
     assert exit_code == 0
 
 
-def two_dimensional_boolean_mask_from_text(text: str) -> NDArray:
-    """
-    Self-explanatory.
-    """
-    result = []
-    for row_text in text.strip().split("\n"):
-        row = list(map(int, row_text))
-        result.append(row)
-    return np.asarray(result, dtype=bool)
-
-
-def two_dimensional_boolean_mask_from_file(path: Path) -> NDArray:
-    """
-    Self-explanatory.
-    """
-    with open(path, "r") as file:
-        return two_dimensional_boolean_mask_from_text(file.read())
-
-
-@pytest.fixture(name="expected_profmask")
-def fixture_expected_profmask() -> NDArray:
-    """
-    Expected profile mask when running clfd on the test archive with:
-    - features: (std, ptp, lfamp)
-    - qmask: 2.0
-    Has shape (num_subints, num_channels).
-    """
-    return two_dimensional_boolean_mask_from_file(Path(__file__).parent / "expected_profmask.txt")
-
-
-@pytest.fixture(name="expected_tpmask")
-def fixture_expected_tpmask() -> NDArray:
-    """
-    Expected time-phase mask when running clfd on the test archive with
-    qspike = 2.0. Has shape (num_subints, num_phase_bins).
-    """
-    return two_dimensional_boolean_mask_from_file(Path(__file__).parent / "expected_tpmask.txt")
-
-
 @pytest.mark.skipif(not HAS_PSRCHIVE, reason="psrchive python bindings must be installed")
 def test_cli_app(
     tmp_path_factory: pytest.TempPathFactory, expected_profmask: NDArray, expected_tpmask: NDArray
