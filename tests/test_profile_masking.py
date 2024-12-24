3f354bfe4f0291814c85a336f1af0a83b013e5d0
Vincent Morello <vmorello@gmail.com>
2024-12-24 17:19:03 +0100

    Phase out old profile masking tests

--- tests/test_profile_masking.py
+++ tests/test_profile_masking.py
@@ -1,75 +1,43 @@
-import inspect
-import os
-import unittest
-from typing import Callable
+from typing import Callable, Iterable
 
-import numpy
+import numpy as np
 import pytest
 from numpy.typing import NDArray
 
-import clfd
-import clfd.features
-
-from .utils import get_example_data_path
-
-
-def load_test_datacube():
-    fname = os.path.join(get_example_data_path(), "npy_example.npy")
-    return numpy.load(fname)
-
-
-def load_features():
-    """Returns a list of tuples (feature_name, featurizer_function)"""
-    return inspect.getmembers(clfd.features, inspect.isfunction)
-
-
-@pytest.fixture(name="data_cube")
-def fixture_data_cube() -> NDArray:
-    fname = os.path.join(get_example_data_path(), "npy_example.npy")
-    return numpy.load(fname)
-
-
-FEATURES: list[tuple[str, Callable]] = inspect.getmembers(clfd.features, inspect.isfunction)
+from clfd import profile_mask2
+from clfd.core import AVAILABLE_FEATURES
 
 
 @pytest.mark.parametrize(
-    "feature", [func for _, func in FEATURES], ids=[name for name, _ in FEATURES]
+    "feature",
+    AVAILABLE_FEATURES.values(),
+    ids=AVAILABLE_FEATURES.keys(),
 )
 def test_feature_returns_ndarray_with_expected_shape(data_cube: NDArray, feature: Callable):
     result = feature(data_cube)
-    assert isinstance(result, numpy.ndarray)
+    assert isinstance(result, np.ndarray)
     assert result.shape == data_cube.shape[:2]
 
 
-class TestFeaturize(unittest.TestCase):
-    def setUp(self):
-        self.cube = load_test_datacube()
-        self.feature_names = [name for name, __ in load_features()]
-        self.num_features = len(self.feature_names)
-
-    def test_featurize(self):
-        for n in range(self.num_features):
-            clfd.featurize(self.cube, features=self.feature_names[: n + 1])
+def test_profile_masking2(data_cube: NDArray, expected_profmask: NDArray):
+    result = profile_mask2(data_cube, features=("std", "ptp", "lfamp"), q=2.0, zap_channels=())
+    assert np.array_equal(result.profile_mask, expected_profmask)
 
 
-class TestProfileMask(unittest.TestCase):
-    def setUp(self):
-        self.cube = load_test_datacube()
-        self.feature_names = [name for name, func in load_features()]
-
-    def test_profile_mask(self):
-        data = clfd.featurize(self.cube, features=self.feature_names)
-
-        # Without specifying zapped channels
-        __, mask = clfd.profile_mask(data, zap_channels=[])
-
-        # With specifying zapped channels
-        # Ensure that those zapped channels are indeed masked in the output mask
-        num_chans = self.cube.shape[1]
-        zap_channels = numpy.arange(0, num_chans, 2)
-        __, mask = clfd.profile_mask(data, zap_channels=zap_channels, q=1000.0)
-        self.assertTrue(numpy.all(mask[:, zap_channels]))
+@pytest.mark.parametrize("zap_channels", [[0], [127], [17, 3, 93, 42], range(42, 93)], ids=repr)
+def test_profile_masking2_with_zapped_channels(data_cube: NDArray, zap_channels: Iterable[int]):
+    result = profile_mask2(data_cube, q=1.0e9, zap_channels=zap_channels)
+    assert set(result.zap_channels) == set(zap_channels)
+    assert result.zap_channels == sorted(result.zap_channels)
+    assert np.all(result.profile_mask[:, zap_channels])
 
 
-if __name__ == "__main__":
-    unittest.main()
+def test_profile_masking2_ignores_out_of_range_zap_channels(data_cube: NDArray):
+    result = profile_mask2(data_cube, q=1.0e9, zap_channels=range(120, 140))
+
+    expected_zap_chanels = list(range(120, 128))
+    assert result.zap_channels == expected_zap_chanels
+
+    is_whole_channel_masked = np.ufunc.reduce(np.logical_and, result.profile_mask, axis=1)
+    assert np.all(is_whole_channel_masked[120:128])
+    assert np.all(np.logical_not(is_whole_channel_masked[0:120]))
